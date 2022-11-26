1ab11e10e2cdf795edc9c10f3497bced74ff7914
magako <qutianh@gmail.com>
2022-11-26 17:36:59 +0900

    add: JWT認証後に利用できるAPIを追加

---
+++ src/main/kotlin/com/example/routes/UserRoutes.kt
@@ -0,0 +1,23 @@
+package com.example.routes
+
+import io.ktor.server.application.call
+import io.ktor.server.auth.authenticate
+import io.ktor.server.auth.jwt.JWTPrincipal
+import io.ktor.server.auth.principal
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
+
+fun Route.userRouting() {
+    route("/user") {
+        authenticate("auth-jwt") {
+            get("/hello") {
+                val principal = call.principal<JWTPrincipal>()
+                val username = principal!!.payload.getClaim("username").asString()
+                val expiresAt = principal.expiresAt?.time?.minus(System.currentTimeMillis())
+                call.respondText("Hello, $username! Token is expired at $expiresAt ms.")
+            }
+        }
+    }
+}
