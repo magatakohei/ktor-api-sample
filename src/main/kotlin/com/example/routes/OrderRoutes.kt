1997ed7e5508310f4f15de7b996491f225b0cd3d
magako <qutianh@gmail.com>
2022-11-27 17:28:29 +0900

    feat: メモリ保存の代わりにDBに保存するように修正

--- src/main/kotlin/com/example/routes/OrderRoutes.kt
+++ src/main/kotlin/com/example/routes/OrderRoutes.kt
@@ -1,25 +1,27 @@
 package com.example.routes
 
-import com.example.models.orderStorage
+import com.example.dao.impl.orderItemDao
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import java.math.BigDecimal
 
 fun Route.listOrdersRoute() {
     get("/order") {
-        if (orderStorage.isNotEmpty()) {
-            call.respond(orderStorage)
-        }
+        call.respond(orderItemDao.all())
     }
 }
 
 fun Route.getOrderRoute() {
     get("/order/{id?}") {
-        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
-        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+        val orderId = call.parameters["id"]?.toIntOrNull() ?: return@get call.respondText(
+            "Bad Request",
+            status = HttpStatusCode.BadRequest
+        )
+        val order = orderItemDao.findByOrderId(orderId) ?: return@get call.respondText(
             "Not Found",
             status = HttpStatusCode.NotFound
         )
@@ -29,12 +31,15 @@ fun Route.getOrderRoute() {
 
 fun Route.totalizeOrderRoute() {
     get("/order/{id?}/total") {
-        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
-        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+        val orderId = call.parameters["id"]?.toIntOrNull() ?: return@get call.respondText(
+            "Bad Request",
+            status = HttpStatusCode.BadRequest
+        )
+        val order = orderItemDao.findByOrderId(orderId) ?: return@get call.respondText(
             "Not Found",
             status = HttpStatusCode.NotFound
         )
-        val total = order.contents.sumOf { it.price * it.amount }
+        val total = order.contents.sumOf { BigDecimal(it.price) * BigDecimal(it.quantity) }
         call.respond(total)
     }
 }
