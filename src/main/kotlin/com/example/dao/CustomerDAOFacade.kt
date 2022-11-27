e43d45d1991c938ca39961b4bd1e6d8ab4720bcf
magako <qutianh@gmail.com>
2022-11-27 13:58:25 +0900

    feat: CustomerDAOを追加

---
+++ src/main/kotlin/com/example/dao/CustomerDAOFacade.kt
@@ -0,0 +1,11 @@
+package com.example.dao
+
+import com.example.models.Customer
+
+interface CustomerDAOFacade {
+    suspend fun allCustomers(): List<Customer>
+    suspend fun customer(id: Int): Customer?
+    suspend fun addNewCustomer(firstName: String, lastName: String, email: String): Customer?
+    suspend fun editCustomer(id: Int, firstName: String, lastName: String, email: String): Boolean
+    suspend fun deleteCustomer(id: Int): Boolean
+}
