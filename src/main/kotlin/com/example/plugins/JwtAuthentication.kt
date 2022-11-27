4f5a684320746d33f837a9db60bedd37e2b078c2
magako <qutianh@gmail.com>
2022-11-27 13:56:21 +0900

    remove: 消し忘れで利用していない変数削除

--- src/main/kotlin/com/example/plugins/JwtAuthentication.kt
+++ src/main/kotlin/com/example/plugins/JwtAuthentication.kt
@@ -29,7 +29,6 @@ import java.util.concurrent.TimeUnit
 
 fun Application.configureJwt() {
     val privateKeyString = environment.config.property("jwt.privateKey").getString()
-    val publicKeyString = environment.config.property("jwt.publicKey").getString()
     val issuer = environment.config.property("jwt.issuer").getString()
     val audience = environment.config.property("jwt.audience").getString()
     val myRealm = environment.config.property("jwt.realm").getString()
